22b81e8e47030dbbdf54b90b7537f1b75e9bc826
luqman13743 <luqman13743@gmail.com>
2026-09-25 14:25:52 +0500

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./db/schema/index.ts",
+  schema: "./db/schema/tables.ts",
   out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
